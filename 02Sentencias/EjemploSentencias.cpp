31df08c1c75c73c1ee98a25a406228bc036f8337
demonassit <jminorg@ipn.mx>
2022-05-25 12:38:50 -0500

    EjemploSentenciaDoWhile

---
+++ 02Sentencias/EjemploSentencias.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	//uso de sentencias de control
+	//do while
+	int i =1;
+	do{
+		//vamos a crear un programa que imprima hasta el 10
+		cout<<i<<endl;
+		i++;
+	}while(i<11);
+}
