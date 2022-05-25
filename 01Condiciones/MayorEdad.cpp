f3f89f09b9aa5417be418570e9df3ae5214a231d
demonassit <jminorg@ipn.mx>
2022-05-25 12:25:49 -0500

    EjemploCondicional

---
+++ 01Condiciones/MayorEdad.cpp
@@ -0,0 +1,16 @@
+//Estructura compuesta o doble
+#include <iostream>
+using namespace std;
+
+main(){
+	int ed;
+	cout<<"Ingrese su edad: ";
+	cin>>ed;
+	if(ed>=18){
+		cout<<"Mayor de edad\n";
+	}else{
+		cout<<"No es mayor de edad\n";
+	}
+	cout<<"Fin del programa";
+	return 0;
+}
