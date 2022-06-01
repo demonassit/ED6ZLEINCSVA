48ac7d663741015dafb7ab949a74ef50632f80cb
demonassit <jminorg@ipn.mx>
2022-06-01 13:20:33 -0500

    lista completa

---
+++ 04Lista/EjemploLista.cpp
@@ -0,0 +1,119 @@
+/*
+Una lista es una estructura de datos que deriva
+una secuancia conectada con nodos y cada yno de los
+cuales cintiene un dato
+Hay un nodo al comienzo llamado la cabeza o frente
+Hay un nodo del termino llamado cola o atras
+
+La lista solo puede ser recorrida en secuencia de
+atras o hacia adelante
+
+Operaciones de su propia estructura
+
+añadir o insertar elemento
+mover atraves de la lista de pri a fin
+buscar elementos
+
+Apuntadores
+
+& memoria
+* al dato
+-> asigna
+
+ejemplo
+
+Nodo *elemento -> &elemento -> valor
+
+en c++ vamso a ocupar list ya que es una clase
+de tipo templete nos permite crear listas
+que contengan cualquier tipo de objeto
+
+la operaciones que se pueden incluir en la lista son:
+
+push meter
+pop sacar
+
+begin()
+end()
+
+size()
+empty()
+
+
+
+
+*/
+
+
+
+#include<iostream>
+
+using namespace std;
+
+//definir la estructura de la lista
+struct Nodo{
+	//los valores de la lista
+	int valor;
+	Nodo *siguiente;
+}
+
+
+//definri los metodos 
+void insertarLista(Nodo *&, int);
+
+int main(){
+	//declarar mi variable de la lista
+	//que apunte a null
+	Nodo *lista = NULL;
+	
+	//variables
+	int op = 1, c, i=0, valores;
+	
+	cout<<"Trabajando con listas(simples, doblemente enlazadas, circulares y circulares dobles)"<<endl;
+	while(op!=3){
+		cout<<"Ejemplo de lista simple: "<<endl;
+		cout<<"1.- Insertar nuevo valor "<<endl;
+		cout<<"2.- Ver la lista "<<endl;
+		cout<<"3.- Salir "<<endl;
+		cin>>op;
+		switch(op){
+			case 1:
+				cout<<"Indica el numero de valores a introducir\n";
+				cin>>valores;
+				while(i<valores){
+					cout<<"Valor["<<i<<"]\n";
+					cin>>c;
+					//insertar el valor
+					insertarLista(lista, c);
+					i++;
+				}
+				break;
+			case 2:
+				cout<<"Mostrar los valores de la lista\n";
+				cout<<"imprimir valores";
+				break;
+			default:
+				cout<<"Gracias por mimir aqui uwu";
+		}
+	}
+	return 0;
+}
+
+void insertarLista(Nodo *&lista, int c){
+	//crear una nueva lista
+	Nodo *inslista = new Nodo();
+	//debo de asignar el valor de la lista
+	inslista->valor = c;
+	
+	//necesitar un auxiliar para encadenar los valores de la lista
+	Nodo *aux = lista;
+	Nodo *aux2;
+	//meter los valores de forma ordenada 
+}
+
+
+
+
+
+
+
---
+++ 04Lista/OtraLista.cpp
@@ -0,0 +1,193 @@
+//lista con apuntadores
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+#include<iostream>
+
+/*
+stdio es la libreria standar de ++
+para el manejo de entradas y salidas en consola
+
+stdlib es la libreria standar de estructuras 
+de datos en c++ ára listas, pilas, colas, arboles
+grafos
+
+conio es la libreria standar para el manejo de 
+manipulacion de la pantalla, pausar, colores,
+manipulacion de ventanas
+*/
+
+using namespace std;
+
+//la definicion de la lista
+
+struct dato{
+	int i;
+	dato *s;//nodo siguiente
+}*a, *i, *p, *e;
+
+//metodos
+
+int buscar(int d);
+void insertar(void);
+void mostrar(void);
+void borrar(void);
+void menu(void);
+//void guardar(void);
+//void cargar(void);
+
+//principal
+main(){
+	menu();
+}
+
+//menu
+
+void menu(void){
+	int opc, da;
+	do{
+		//imprimir le menu
+		cout<<"Ejemplo de una lista con apuntadores";
+		cout<<"\n 1.- Buscar datos:";
+		cout<<"\n 2.- Insertar datos:";
+		cout<<"\n 3.- Motrar datos:";
+		cout<<"\n 4.- Borrar dato:";
+		cout<<"\n 5.- Guardar datos en un archivo:";
+		cout<<"\n 6.- Cargar datos en un archivo:";
+		cout<<"\n 7.- Salir";
+		cout<<"\n Seleccione la opcion deseada.";
+		cin>>opc;
+		
+		switch(opc){
+			case 1:
+				//buscar
+				cout<<"\n Ingresa el dato a buscar: ";
+				cin>>da;
+				if(buscar(da)){
+					cout<<"Datos si existe wiiii";
+				}else{
+					cout<<"Dato no existe solo juguito contigo T_T";
+					getch(); //pausa
+				}
+				break;
+				
+			case 2:
+				//ingresar
+				cout<<"\n Ingresa el dato:";
+				cin>>da;
+				insertar(da);
+				break;
+			
+			case 3:
+				//mostrar
+				mostrar();
+				break;
+				
+			case 4:
+				//borrar
+				borrar();
+				break;
+				
+			case 5:
+				//guardar
+				//guardar();
+				break;
+				
+			case 6: 
+				//cargar
+				//cargar();
+				break;
+			
+			case 7: 
+				//salir
+				cout<<"\n Ayos, gracias uwu";
+				getch;
+				//antes de salir del programa
+				//hay que borrar todos los datos
+				//de la lista
+				p = i;
+				while(p){
+					a = p;
+					p = p->s;
+					delete(a);	
+				}
+				//salir
+				exit(0);
+				
+			default:
+				cout<<"Aprenda a leer, opcion no valida";
+				getch();
+				
+				
+		}
+	}while(opc);
+}
+
+
+//mostrar
+
+void mostrar(void){
+	int cont = 1;
+	//saber si hay una lista
+	if(!i){
+		cout<<"No hay lista parar mostrar";
+		getch();
+		return;
+	}
+	p = i;
+	cout<<"\n\n";
+	/*
+	Como si existe un datos para la lista
+	tenermos que empezar a recorrerarla
+	por cada uno de los elementos
+	*/
+	while(p){
+		cout<<cont++<<"\nValor : "<<p->i<<enld;
+		p = p -> s;
+	}
+	cout<<"\n Fin de la lista";
+	getch();
+}
+
+//buscar
+int buscar(int d){
+	//saber si hay una lista
+	if(!i){
+		cout<<"No hay datos en la lista parar mostrar";
+		getch();
+		return;
+	}
+	
+	//si hay
+	p = i;
+	a = NULL;
+	
+	while(p->s && p->i < d){
+		//recorrer los valores de la lista
+		a = p;
+		p = p->s;
+	}
+	
+	return (p->i == d?1:0);
+}
+
+/*
+para insertar cuantos caminos hay?
+1.- la primera vez que se crea un dato
+
+se debe de crear el nodo y verificar los apuntadores
+
+2.- enmedio
+
+se debe de buscar donde se va a ingresar
+debe de acomodar el apuntador anterior
+y el apuntador siguiente (apuntador que apunta)
+
+3.- final solo se inserta y se ajusta el apuntador
+
+
+
+
+*/
